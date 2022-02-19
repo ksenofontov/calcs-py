41c3b2c32482482c4d5db977bb884b7c6261af6a
Денис Ксенофонтов <Denis@MacBook-Air-Denis.local>
2022-02-19 14:01:10 +0300

    init3

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ class Calculator:
     self.records = []
     self.today = datetime.now()
     self.limit = limit
+    USD_RATE = 75
+    EUR_RATE = 90
+
 
   def add_record(self, record):
     self.records.append(record)
@@ -27,20 +30,36 @@ class Calculator:
     for record in self.records:
       if record.date.day == self.today.day:
         count += record.amount
-    print(count)
+    return count
 
   def get_week_stats(self):
     count = 0
     for record in self.records:
-      if self.record.date
+      if self.today.week - 7 < record.date.week <= self.today.week:
+          count += record.amount
+    return count
 
 class CaloriesCalculator(Calculator):
   def get_calories_remained(self):
-
+    spent = self.get_today_stats()
+    if spent < self.limit:
+      return "Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более " + (self.limit - spent) + " кКал"
+    elif spent >= self.limit:
+      return "Хватит есть!"
 
 
 class CashCalculator(Calculator):
-  def get_today_cash_remained(currency):
+
+  def get_today_cash_remained(self,currency):
+    spent = self.get_today_stats()
+    if spent < self.limit:
+      return ("На сегодня осталось" + (self.limit - spent) + currency)
+    elif spent == self.limit:
+      return "Денег нет, держись"
+    elif spent > self.limit:
+      return "Денег нет, держись: твой долг -" (spent - self.limit) + currency
+
+
 
 
 
