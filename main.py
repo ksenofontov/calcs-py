17886d26f5ec71143d674d7c8f90516cfbe74fb2
Denis <dennisius@yahoo.com>
2022-02-20 13:13:11 +0300

    init6

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ class Calculator:
         self.limit = limit
 
     def add_record(self, record):
+        """Добавляет запись в список."""
         self.records.append(record)
 
     def get_today_stats(self):
+        """Возвращает количество потраченных за сегодняшний день единиц"""
         count = 0
         for record in self.records:
             if record.date.day == self.today.day:
@@ -25,8 +27,8 @@ class Calculator:
         return count
 
     def get_week_stats(self):
+        """Возвращает количество потраченных за неделю единиц"""
         count = 0
-
         for record in self.records:
             if self.today.day * 7 - 7 < record.date.day * 7 <= self.today.day * 7:
                 count += record.amount
@@ -35,6 +37,7 @@ class Calculator:
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
+        """Возвращает ответ об оставшихся калориях в зависимости от лимита"""
         spent = self.get_today_stats()
         if spent < self.limit:
             return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit - spent} кКал"
@@ -48,6 +51,7 @@ class CashCalculator(Calculator):
         self.CUR = {'usd': 1/75, 'eur': 1/90, 'rub': 1}
 
     def get_today_cash_remained(self, currency):
+        """Возвращает ответ об оставшихся средствах в зависимости от лимита"""
         spent = self.get_today_stats()
         if spent < self.limit:
             return f"На сегодня осталось {(self.limit - spent)*self.CUR[currency]} {currency}"
